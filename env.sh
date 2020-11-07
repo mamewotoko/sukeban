094beb5b429edb84846d0ce21ee8426d965fdd69
vagrant <vagrant@vagrant.vm>
2020-11-07 05:34:36 +0000

    use [[

--- env.sh
+++ env.sh
@@ -2,8 +2,8 @@ export UNAME="$(uname)"
 
 if [ "$UNAME" = Darwin ]; then
     export PATH="/usr/local/opt/make/libexec/gnubin:$PATH"
-elif [[ "$UNAME" == "MSYS_NT"* ]]; then
+elif [ "$UNAME" = "MSYS_NT"* ]; then
     export PATH=/mingw64/bin:$PATH
     export LD_LIBRARY_PATH=/mingw64/lib:$LD_LIBRARY_PATH
     export PKG_CONFIG_PATH=/mingw64/lib/pkgconfig
-fi     
+fi
--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 
 if [ "$UNAME" = Darwin ]; then
     brew update
-    brew install gtk+3 
+    brew install gtk+3
     # make gcc pkg-config
 
 elif [ -f /etc/lsb-release ]; then
@@ -15,7 +15,7 @@ elif [ -f /etc/redhat-release ] && grep "release 7" /etc/redhat-release; then
     sudo yum install -y gcc gobject-introspection-devel \
          pkg-config gtk3 make
 
-elif [[ "$UNAME" == "MINGW64_NT"* ]]; then
+elif [ "$UNAME" = "MINGW64_NT"* ]; then
     # msys2 on PC
     pacman -Syu --noconfirm
     pacman -Sy --noconfirm mingw-w64-x86_64-gtk3 \
@@ -23,8 +23,8 @@ elif [[ "$UNAME" == "MINGW64_NT"* ]]; then
            mingw-w64-x86_64-gcc \
            mingw-w64-x86_64-pkg-config
 
-elif [[ "$UNAME" == "MSYS_NT"* ]]; then
-    # travis 
+elif [ "$UNAME" = "MSYS_NT"* ]; then
+    # travis
     $msys2 pacman -Syu --noconfirm
     $msys2 pacman -Sy --noconfirm mingw-w64-x86_64-gtk3 \
            mingw-w64-x86_64-make \
