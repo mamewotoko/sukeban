b77b32596b6000c3442d80e37a9c69a8b5542af4
Takashi Masuyama <mamewotoko@gmail.com>
2020-11-07 14:36:01 +0900

    use [[

--- setup.sh
+++ setup.sh
@@ -1,21 +1,21 @@
 #! /bin/bash
 . ./env.sh
 
-if [ "$UNAME" = Darwin ]; then
+if [[ "$UNAME" = Darwin ]]; then
     brew update
     brew install gtk+3
     # make gcc pkg-config
 
-elif [ -f /etc/lsb-release ]; then
+elif [[ -f /etc/lsb-release ]]; then
     sudo apt-get update
     sudo apt-get install -y libgtk-3-dev pkg-config \
          xvfb make gcc
 
-elif [ -f /etc/redhat-release ] && grep "release 7" /etc/redhat-release; then
+elif [[ -f /etc/redhat-release ]] && grep "release 7" /etc/redhat-release; then
     sudo yum install -y gcc gobject-introspection-devel \
          pkg-config gtk3 make
 
-elif [ "$UNAME" = "MINGW64_NT"* ]; then
+elif [[ "$UNAME" = "MINGW64_NT"* ]]; then
     # msys2 on PC
     pacman -Syu --noconfirm
     pacman -Sy --noconfirm mingw-w64-x86_64-gtk3 \
@@ -23,7 +23,7 @@ elif [ "$UNAME" = "MINGW64_NT"* ]; then
            mingw-w64-x86_64-gcc \
            mingw-w64-x86_64-pkg-config
 
-elif [ "$UNAME" = "MSYS_NT"* ]; then
+elif [[ "$UNAME" = "MSYS_NT"* ]]; then
     # travis
     $msys2 pacman -Syu --noconfirm
     $msys2 pacman -Sy --noconfirm mingw-w64-x86_64-gtk3 \
